ea1d7e9d3b4177879d115fca07b72b2c8fe54e05
xcode73 <xcode.uln@gmail.com>
2023-07-03 14:12:35 +0400

    feat: add ability to show/hide DetailTableView rows

--- ToDo List/ToDoDetailTableViewController.swift
+++ ToDo List/ToDoDetailTableViewController.swift
@@ -12,19 +12,33 @@ class ToDoDetailTableViewController: UITableViewController {
     @IBOutlet weak var saveBarButton: UIBarButtonItem!
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var datePicker: UIDatePicker!
-    @IBOutlet weak var noteView: UITextView!
+    @IBOutlet weak var noteView: UITextView!    
+    @IBOutlet weak var remainderSwitch: UISwitch!
+    @IBOutlet weak var dateLabel: UILabel!
     
     var toDoItem: ToDoItem!
+    
+    let datePickerIndexPath = IndexPath(row: 1, section: 1)
+    let notesTextVeiwIndexPath = IndexPath(row: 0, section: 2)
+    let notesRowHeight: CGFloat = 200
+    let defaultRowHeight: CGFloat = 44
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if toDoItem == nil {
-            toDoItem = ToDoItem(name: "", date: Date(), notes: "")
+            toDoItem = ToDoItem(name: "", date: Date(), notes: "", remainderSet: false)
         }
+        
+        updateUserInterface()
+    }
+    
+    func updateUserInterface() {
         nameField.text = toDoItem.name
         datePicker.date = toDoItem.date
         noteView.text = toDoItem.notes
+        remainderSwitch.isOn = toDoItem.remainderSet
+        dateLabel.textColor = (remainderSwitch.isOn ? .black : .gray)
     }
 
     @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
@@ -37,7 +51,26 @@ class ToDoDetailTableViewController: UITableViewController {
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        toDoItem = ToDoItem(name: nameField.text!, date: datePicker.date, notes: noteView.text)
+        toDoItem = ToDoItem(name: nameField.text!, date: datePicker.date, notes: noteView.text, remainderSet: remainderSwitch.isOn)
         
+    }
+    
+    @IBAction func remainderSwitchChanged(_ sender: Any) {
+        dateLabel.textColor = (remainderSwitch.isOn ? .black : .gray)
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
+}
+
+extension ToDoDetailTableViewController {
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case datePickerIndexPath:
+            return remainderSwitch.isOn ? datePicker.frame.height : 0
+        case notesTextVeiwIndexPath:
+            return notesRowHeight
+        default:
+            return defaultRowHeight
+        }
     }
 }
--- ToDo List/ToDoItem.swift
+++ ToDo List/ToDoItem.swift
@@ -11,4 +11,5 @@ struct ToDoItem: Codable {
     var name: String
     var date: Date
     var notes: String
+    var remainderSet: Bool
 }
